b231527bc95f69e746a1f2862e34a683a2f313b7
hzycoder <434741598@qq.com>
2017-05-13 17:07:40 +0800

    register

--- WebRoot/register.jsp
+++ WebRoot/register.jsp
@@ -21,27 +21,28 @@
 			<legend>请填写以下信息</legend>
 			<s:if test="hasFieldError()"></s:if>
 			<s:fielderror />
+			<s:property value="#session.birthdayError"/>
 			<s:property value="#session.RegSUCCESS"></s:property>
-			<s:form action="regAction" method="post" namespace="/" theme="simple">
+			<s:form action="regAction!add.action" method="post" namespace="/" theme="simple">
 				<br>
-				用户名：<s:textfield name="username" value="123" label="用户名" />
+				*用户名：<s:textfield name="username" value="123" label="用户名" maxlength="20"/>
 				<br>
 				<br>
-				密&nbsp;&nbsp;码：<s:password name="password" value="123" label="密码" />
+				*密&nbsp;&nbsp;码：<s:password name="password" value="123" label="密码" />
 				<br>
 				<br>
 				生&nbsp;&nbsp;日:<s:select name="year"
-					list="{'','2017','2016','2015','2014','2013','2012','2011','2010','2009','2008','2007',
+					list="{'2017','2016','2015','2014','2013','2012','2011','2010','2009','2008','2007',
 		'2006','2005','2004','2003','2002','2001','2000','1999','1998','1997','1996','1995','1994','1993',
 		'1992','1991','1990','1989','1988','1987','1986','1985','1984','1983','1982','1981','1980','1979',
 		'1978','1977','1976','1975'}"
-					value="2017" />
+					headerKey="" headerValue="-请选择-" />
 				<s:select name="month"
-					list="{'','12','11','10','09','08','07','06','05','04','03','02','01'}"
-					value="08" />
+					list="{'12','11','10','09','08','07','06','05','04','03','02','01'}"
+					headerKey="" headerValue="-请选择-" />
 				<s:select name="day"
-					list="{'','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','21','22','23','24','25','26','27','28','29','30','31'}"
-					value="08" />
+					list="{'01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','21','22','23','24','25','26','27','28','29','30','31'}"
+					headerKey="" headerValue="-请选择-"/>
 				<br>
 				<br>
 				真&nbsp;&nbsp;名：<s:textfield name="petname" value="狗子"></s:textfield>
--- src/com/action/RegisterAction.java
+++ src/com/action/RegisterAction.java
@@ -3,20 +3,23 @@ package com.action;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
 import org.apache.struts2.components.Else;
 
 import com.domain.TUser;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.service.RegisterService;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
-public class RegisterAction extends ActionSupport{
+public class RegisterAction extends ActionSupport {
 	@Resource
 	RegisterService regSrv;
-	
+
 	private String username;
 	private String password;
 	private Date birthday = null;
@@ -35,129 +38,142 @@ public class RegisterAction extends ActionSupport{
 		return year;
 	}
 
-
 	public void setYear(String year) {
 		this.year = year;
 	}
-
 
 	public String getMonth() {
 		return month;
 	}
 
-
 	public void setMonth(String month) {
 		this.month = month;
 	}
-
 
 	public String getDay() {
 		return day;
 	}
 
-
 	public void setDay(String day) {
 		this.day = day;
 	}
-
 
 	public String getUsername() {
 		return username;
 	}
 
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	public Date getBirthday() {
 		return birthday;
 	}
 
-
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
 
-	
 	public String getPetname() {
 		return petname;
 	}
-
 
 	public void setPetname(String petname) {
 		this.petname = petname;
 	}
 
-
 	public String getCollege() {
 		return college;
 	}
-
 
 	public void setCollege(String college) {
 		this.college = college;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+	public void validateAdd() {
+		Pattern numPatt = Pattern.compile("[0-9]*");
+		Pattern emailPatt = Pattern.compile("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+		if (!year.equals("") || !month.equals("") || !day.equals("")) {
+			if(year.equals("") || month.equals("") || day.equals("")){
+				this.addFieldError("birthdayError", "请填写完整生日日期");				
+			}
+		}
+		if (!email.equals("")&&!emailPatt.matcher(email).matches()) {
+			this.addFieldError("emailError", "请正确填写邮箱格式");
+		}
+		byte[] bytes = username.getBytes();
+		if(bytes.length>20){
+			this.addFieldError("usernameError", "用户名长度过长");
+		}
+		byte[] bytes1 = password.getBytes();
+		if(bytes1.length>20){
+			this.addFieldError("passwordError", "密码长度过长");
+		}
+		if (phone.length() != 11) {
+			this.addFieldError("phoneError", "请填写正确的联系电话");
+		}
+		if (!numPatt.matcher(phone).matches()) {
+			this.addFieldError("phoneError", "请填写正确的联系电话");
+		}
 
-	@Override
-	public String execute() throws Exception {
+	}
+
+	public String add() throws Exception {
 		ActionContext ac = ActionContext.getContext();
 
-		TUser user = new TUser(username, password, birthday, background, uPicture, petname, college, email, phone,registerTime);
-		//整合生日日期
-		String birthdayString = year+month+day;
+		TUser user = new TUser(username, password, birthday, background, uPicture, petname, college, email, phone,
+				registerTime);
+		// 整合生日日期
+		String birthdayString = year + month + day;
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		Date date = sdf.parse(birthdayString);
 		user.setBirthday(date);
-		
-		System.out.println("date日期:::"+user.getBirthday().toString());
-		System.out.println("user==!!!!!!!==="+user.toString());
+
+		System.out.println("date日期:::" + user.getBirthday().toString());
+		System.out.println("user==!!!!!!!===" + user.toString());
 		System.out.println("注册EXECUTE");
-		
+
 		Integer id = regSrv.insertUser(user);
-		if(id>0){
+		if (id > 0) {
 			ac.getSession().put("RegSUCCESS", "注册成功");
 			return SUCCESS;
-		}else if(id==-1){
+		} else if (id == -1) {
 			this.addFieldError(ERROR, "该用户名已注册");
 			return INPUT;
-		}else
+		} else {
 			this.addFieldError(ERROR, "网站繁忙");
 			return INPUT;
-		
+		}
+
 	}
-	
+
+	@Override
+	public String execute() throws Exception {
+		return INPUT;
+	}
 
 }
